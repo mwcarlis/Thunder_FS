e569fb9a3059be87e6c49f4ee6506a26d52d9aaa
mwcarlis <matthew.carlis@sjsu.edu>
2014-11-15 19:37:38 -0800

    Added logic to fill_super block and fixed the arguments Added tree_descr for static compile time file creation

--- thunder_fs.c
+++ thunder_fs.c
@@ -5,6 +5,7 @@
 #include <asm/atomic.h>
 #include <asm/uaccess.h>
 
+#define TUNDER_MAGIC 0x19980122
 
 // Boilerplate 
 MODULE_LICENSE("GPL");
@@ -29,21 +30,38 @@ static ssize_t thunder_write_file(struct file *flip, const char *buf,
 }
 
 
-static struct file_operations(){
+static struct thunder_file_ops(){
 	.open = thunder_open,
 	.read = thunder_read_file,
 	.write = thunder_write_file,
 };
 
+struct tree_descr ThunderFiles[] = {
+	{NULL, NULL, 0}, // Skipped
+	{	.name = "counter0",
+		.ops = &thunder_file_ops,
+		.mode = S_IWURS | S_IRUGO },
+	{	.name = "counter1",
+		.ops = &thunder_file_ops,
+		.mode = S_IWUSR | S_IRUGO },
+	{	.name = "counter2",
+		.ops = &thunder_file_ops,
+		.mode = S_IWUSR | S_IRUGO },
+	{	.name = "counter3",
+		.ops = &thunder_file_ops,
+		.mode = S_IWUSR | S_IRUGO },
+	{ 	"", NULL, 0}
+};
+
 // Fill a superblock with stuff
-static int thunder_fill_super(struct file_system_type *fst,
-				int flags, const char *devname, void *data){
-	
+static int thunder_fill_super(struct super_block *sb, void *data, int silent ){
+	return simple_fill_super(sb, THUNDER_MAGIC, ThunderFiles);
 }
 
 // Stuff to pass in when registering the filesystem
 static struct super_block *thunder_get_super(struct file_system_type *fst, 
 					int flags, const char *devname, void *data){
+	return get_sb_single(fst, flags, lfs_fill_super);
 }
 
 static struct file_system_type thunder_type = {
@@ -56,9 +74,12 @@ static struct file_system_type thunder_type = {
 
 // Get things up
 static int __init thunder_init(void){
+	// hangs thunder_type in the chain with head `file_systems`.
+	return register_filesystem(&thunder_type);
 }
 
 static void __exit thunder_exit(void){
+	// unhangs thunder_type from the chain with head `file_systems`.
 	unregister_filesystem(&thunder_type);
 }
 
